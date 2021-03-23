e796cd101cee67b31051f7b9d2d842bd1f38880c
washington <thewashington.souza@gmail.com>
2021-03-23 16:52:22 -0300

    feat: add footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2020
+      <a href='http://cod3rcources.com.br' target='_blank'>Course</a>
+    </strong>
+  </footer>
+)
