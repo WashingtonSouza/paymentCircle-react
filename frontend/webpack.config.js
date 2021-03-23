2e3ba46aecb1ae7587437927dc95c66955a41c58
washington <thewashington.souza@gmail.com>
2021-03-23 16:25:46 -0300

    feat: implement lte template and create sideBar component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/index.jsx',
@@ -15,7 +15,7 @@ module.exports = {
     extensions: ['', '.js', '.jsx'],
     alias: {
       modules: __dirname + '/node_modules',
-      jquery: 'modules/admin-lte/plugins/jquery/jquery-2.2.3.min.js',
+      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
   },
@@ -25,13 +25,13 @@ module.exports = {
       jQuery: 'jquery',
       'window.jQuery': 'jquery'
     }),
-    new ExtractTextPlugin('css')
+    new ExtractTextPlugin('app.css')
   ],
   module: {
     loaders: [{
       test: /.js[x]?$/,
       loader: 'babel-loader',
-      exclude: /node_modules/,
+      exclude: '/node_modules/',
       query: {
         presets: ['es2015', 'react'],
         plugins: ['transform-object-rest-spread']
