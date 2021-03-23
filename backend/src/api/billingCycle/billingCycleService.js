9baac2847b7119dc174c59c1352c99c93a934942
washington <thewashington.souza@gmail.com>
2021-03-22 23:02:32 -0300

    feat: finishing backend and moving files to backend folder

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,35 @@
+const BillingCyle = require('./billingCycle')
+const errorHandler = require('../commons/errorHandler')
+
+BillingCyle.methods(['get', 'post', 'put', 'delete'])
+BillingCyle.updateOptions({ new: true, ruValidators: true })
+BillingCyle.after('post', errorHandler).after('put', errorHandler)
+
+BillingCyle.route('count', (req, res, next) => {
+  BillingCyle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value })
+    }
+  })
+})
+
+BillingCyle.route('summary', (req, res, next) => {
+  BillingCyle.aggregate({
+    $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+  }, {
+    $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+  }, {
+    $project: { _id: 0, credit: 1, debt: 1 }
+  }, (error, result) => {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json(result[0] || { credit: 0, debt: 0 })
+    }
+  })
+})
+
+
+module.exports = BillingCyle
