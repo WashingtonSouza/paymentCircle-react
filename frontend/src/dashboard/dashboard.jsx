00a21597707f2ca6f92a267c496f5b43b886fb4f
washington <thewashington.souza@gmail.com>
2021-03-24 16:19:13 -0300

    feat: add valueBox component

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
 
 
 class Dashboard extends Component {
@@ -10,7 +11,9 @@ class Dashboard extends Component {
       <div>
         <ContentHeader title='Dashboard' small='Version 1.0' />
         <Content>
-          Dashboard
+          <ValueBox cols='12 4' color='green' icon='bank' value='10' text='Credits amount' />
+          <ValueBox cols='12 4' color='red' icon='credit-card' value='10' text='Debt amount' />
+          <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Consolited amount' />
         </Content>
       </div>
     )
