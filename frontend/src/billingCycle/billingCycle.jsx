25bb9793c49e17003f4cd3ce06393a943b23cd95
washington <thewashington.souza@gmail.com>
2021-03-25 17:26:52 -0300

    feat: add tabs in billingCycle and connect with redux

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
-import TabsContent from '../common/tab/tabContent'
+import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
-
+import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab('tabList')
+  }
+
   render() {
     return (
       <div>
@@ -22,7 +29,10 @@ class BillingCycle extends Component {
               <TabHeader label='Delete' icon='trasg-o' target='tabDelete' />
             </TabsHeader>
             <TabsContent>
-
+              <TabContent id='tabList'><h1>List</h1></TabContent>
+              <TabContent id='tabAdd'><h1>Create</h1></TabContent>
+              <TabContent id='tabEdit'><h1>Edit</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Delete</h1></TabContent>
             </TabsContent>
           </Tabs>
         </Content>
@@ -31,4 +41,5 @@ class BillingCycle extends Component {
   }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
