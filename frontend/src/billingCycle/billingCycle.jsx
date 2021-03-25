61783a6c5a8bdff82883f4756bb6239eed21f404
washington <thewashington.souza@gmail.com>
2021-03-25 16:12:39 -0300

    feat: integrate billingCycle with redux and push content from backend

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <div>
-    <h1>Payment cycle</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class BillingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Payment Cycle' small='Register' />
+        <Content>
+          Payment Cycle test
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default BillingCycle
--- frontend/src/dashboard/dashboardReducer.js
+++ frontend/src/dashboard/dashboardReducer.js
@@ -1,5 +1,12 @@
+import { actionTypes } from "redux-form"
+
 const INICITAL_STATE = { summary: { credit: 0, debt: 0 } }
 
-export default function (state = INICITAL_STATE) {
-  return state
+export default function (state = INICITAL_STATE, action) {
+  switch (action.type) {
+    case 'BILLING_SUMMARY_FETCHED':
+      return { ...state, summary: action.payload.data }
+    default:
+      return state
+  }
 }
