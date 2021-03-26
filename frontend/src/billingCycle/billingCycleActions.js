4a9fe0e810766eac26198f04ce402350670c852a
washington <thewashington.souza@gmail.com>
2021-03-26 19:01:49 -0300

    feat: add actions as clear form and back to list from add

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 
 const BASE_URL = 'http://localhost:3004/api'
 
@@ -12,14 +16,19 @@ export function getList() {
 }
 
 export function create(values) {
-  axios.post(`${BASE_URL}/billingCycles`, values)
-    .then(resp => {
-      toastr.success('Sucess', 'Operation saved successfully')
-    })
-    .catch(e => {
-      e.response.data.errors.forEach(error => toastr.error('Error', error));
-    })
-  return {
-    type: 'TEMP'
+  return dispatch => {
+    axios.post(`${BASE_URL}/billingCycles`, values)
+      .then(resp => {
+        toastr.success('Sucess', 'Operation saved successfully')
+        dispatch([
+          resetForm('billingCycleForm'),
+          getList(),
+          selectTab('tabList'),
+          showTabs('tabList', 'tabAdd')
+        ])
+      })
+      .catch(e => {
+        e.response.data.errors.forEach(error => toastr.error('Error', error));
+      })
   }
 }
